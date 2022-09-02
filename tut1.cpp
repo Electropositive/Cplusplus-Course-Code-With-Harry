5fff1be657117afb36f8469c8ebbad51e8bd691f
Electropositive <76994604+Electropositive@users.noreply.github.com>
2022-09-02 12:42:31 +0530

    added tutorial 1 cpp file from local machine

---
+++ tut1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+// int c = 58;
+
+int main()
+{
+    //******** Build in data types********
+    // int a, b, c;
+    // cout<<"Enter the value of a: "<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of b: "<<endl;
+    // cin>>b;
+    // c = a+b;
+    // cout<<"The sum of a and b is: "<<c<<endl;
+    // cout<<"The global c is: "<<::c<<endl;
+
+    //********Float, Double and Long Double Literals********
+    // float d = 3.14f;
+    // long double e = 3.14l;
+
+    // cout<<"The size of 3.14 is: "<<sizeof(3.14)<<endl;
+    // cout<<"The size of 3.14f is: "<<sizeof(3.14f)<<endl;
+    // cout<<"The size of 3.14F is: "<<sizeof(3.14F)<<endl;
+    // cout<<"The size of 3.14l is: "<<sizeof(3.14l)<<endl;
+    // cout<<"The size of 3.14L is: "<<sizeof(3.14L)<<endl;
+    // cout<<"The size of d is: "<<d<<endl<<"The size of e is: "<<e<<endl;
+
+    //********Reference Variables********
+    // here y is a refernce variable Pointing towards x
+
+    // float x = 455;
+    // float & y = x;
+    // cout<<x<<endl;
+    // cout<<y<<endl;
+
+    //********Typecasting********
+    //changing one variable type to another
+
+    // int a = 45;
+    // float b = 45.45;
+
+    // cout<<"The value of a is: "<<(float)a<<endl;
+    // cout<<"The value of a is: "<<float(a)<<endl;
+
+    // cout<<"The value of b is: "<<(int)b<<endl;
+    // cout<<"The value of b is: "<<int(b)<<endl;
+
+    // int c = int(b);
+
+    // cout<<"The expression is: "<< a + b <<endl;
+    // cout<<"The expression is: "<< a + int(b) <<endl;
+    // cout<<"The expression is: "<< a + (int)b <<endl;
+
+    return 0;
+}
