8ab42be4d40a00119323a8c50ed998591299fad1
Electropositive <76994604+Electropositive@users.noreply.github.com>
2022-09-02 12:44:12 +0530

    added tutorial 8 cpp file from local machine (skipped 2-7)

---
+++ tut8.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    // int a = 35;
+    // cout<<"The value of a was: "<<a<<endl;
+    // a = 48;
+    // cout<<"The value of a is: "<<a<<endl;
+
+    //********Constants********
+    // here value of a will never change throughout the code
+
+    // const int a = 35;
+    // cout<<"The value of a was: "<<a<<endl;
+    // a = 48;
+    // cout<<"The value of a is: "<<a<<endl;
+
+    //********Manipulators in C++********
+    // Helps format display text
+
+    // int a = 3, b = 76, c = 1234;
+
+    // cout<<"The value of a without setw is: "<<a<<endl;
+    // cout<<"The value of a without setw is: "<<b<<endl;
+    // cout<<"The value of a without setw is: "<<c<<endl<<endl;
+    // // setw-- set width
+    // cout<<"The value of a with setw is: "<<setw(4)<<a<<endl;
+    // cout<<"The value of a with setw is: "<<setw(4)<<b<<endl;
+    // cout<<"The value of a with setw is: "<<setw(4)<<c<<endl;
+
+    //********Operater Precedence********
+
+    // int a = 3, b = 4;
+    // int c = ((((a*5)+b)-45)+87);
+    // cout<<c;
+
+
+    return 0;
+}
