b37b5394f049d63c489f3d9b3dca4285d4a6d681
Electropositive <76994604+Electropositive@users.noreply.github.com>
2022-09-02 12:46:50 +0530

    added tutorial 10 cpp file from local machine

---
+++ tut10.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    /*  Loops in c++
+        There are three types of loops in C++ :
+        1.) For loop
+        2.) While loop
+        3.) Do-While loop
+    */
+
+    /********For Loop in C++*********/
+    // int n;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    /********Do - While Loop in C++*********/
+    // Question: Print numbers from 1 to 5 then 10 to 15 then 20 to 25.
+
+    // for (int i = 1; i <= 25; i++)
+    // {
+    //     if (i >= 1 && i <= 5)
+    //     {
+    //         cout << i << endl;
+    //     }
+    //     if (i >= 10 && i <= 15)
+    //     {
+    //         cout << i << endl;
+    //     }
+    //     if (i >= 20 && i <= 25)
+    //     {
+    //         cout << i << endl;
+    //     }
+    //  }
+
+    // while (i<=5)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // while (i>=10 && i <= 15)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // while (i>= 20 && i <= 25)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+
+    // multiplication table for 6
+    // int i = 1;
+    // while (i <= 10)
+    // {
+    //     cout << i * 6 << endl;
+    //     i++;
+    // }
+
+    return 0;
+}
+
+/*
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
