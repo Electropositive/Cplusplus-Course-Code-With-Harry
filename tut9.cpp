c637d6ae3f7e2f93e1c4ff580b617bf9f82ec05f
Electropositive <76994604+Electropositive@users.noreply.github.com>
2022-09-02 12:45:01 +0530

    added tutorial 9 cpp file from local machine

---
+++ tut9.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //********Selection Control Structure If - else statements********
+
+    // int age;
+    // cin >> age;
+
+    // if (age < 18)
+    // {
+    //     cout << "You cannot come to my party" << endl;
+    // }
+    // else if (age > 18)
+    // {
+    //     cout << "You can come to my party" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Kids pass is required" << endl;
+    // }
+
+    //********Selection Control Structure: Switch statements********
+    // switch (age)
+    // {
+    // case 18:
+    //     cout<<"You are of 18 age"<<endl;
+    //     break;
+    // case 19:
+    //     cout<<"You are of 19 age"<<endl;
+    //     break;
+    // case 20:
+    //     cout<<"You are of 20 age"<<endl;
+    //     break;
+    // case 21:
+    //     cout<<"You are of 21 age"<<endl;
+    //     break;
+    
+    // default:
+    //     break;
+    // }
+
+    return 0;
+}
