451f2708de86f44726a27cfc499c252a3241dc57
Electropositive <76994604+Electropositive@users.noreply.github.com>
2022-09-02 12:47:43 +0530

    added tutorial 12 cpp file from local machine

---
+++ tut12.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Pointers in C++ */
+
+    // int a;
+    // cout<<"Input a value to find its address: ";
+    // cin>>a;
+    // cout<<endl;
+    // cout<<"******************************************";
+    // int *b = &a;
+
+    // // & ----> (Address of) Operater
+    // cout<<"The address of a is: "<<&a<<endl;
+    // cout<<"The address of a is: "<<b<<endl;
+
+    // // * ----> (value at) Derefrence Operater
+    // cout<<"The value at address b is: "<<*b<<endl<<endl;
+
+    // // Pointer to Pointer 
+    // // store address of a pointer
+    // int ** c = &b;
+    // cout<<"The address of b is: "<<&b<<endl;
+    // cout<<"The address of b is: "<<c<<endl<<endl;
+    
+    // cout<<"The value at address c is: "<<*c<<endl;
+    // cout<<"The value at address value_at(value_at(c)) is: "<<**c<<endl;
+
+    return 0;
+}
